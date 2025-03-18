5ca466de3de2dcd78b1446b069b9490da141ca23
Safia Jama <safiajama@Safias-Laptop.local>
2025-03-18 02:52:04 +0000

    add a Post model

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 
+
 # Create your views here.
+
 
 def my_blog(request):
     return HttpResponse("Hello, blog!")
--- blogger/settings.py
+++ blogger/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(r1c2rnvukbtd0wf&mbyt@q6clzjc7&x+_$4)qwg&)n10!5=6y'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 'https://djangoprojectf-86dd66b321f1.herokuapp.com',
+                 'http://127.0.0.1:8000',
+                 '127.0.0.1', 
+                 'localhost']
 
 
 # Application definition
@@ -74,14 +82,21 @@ WSGI_APPLICATION = 'blogger.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000/blog/",
+    "https://djangoprojectf-86dd66b321f1.herokuapp.com"
+]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
